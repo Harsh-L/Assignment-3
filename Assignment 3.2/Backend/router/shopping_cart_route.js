bb37e8978de054d6bbb005bc4d355905e8b14eae
Harsh Lad <68841816+Harsh-L@users.noreply.github.com>
2022-10-27 08:47:39 +0530

    designed routing structure

--- Assignment 3.2/Backend/router/shopping_cart_route.js
+++ Assignment 3.2/Backend/router/shopping_cart_route.js
@@ -1,4 +1,46 @@
 var router = require('express').Router();
 
 
-router.get('/', (req,res) => {});
+router.get('/', async (req,res) => {
+
+});
+
+router.get('/login', (req,res) => {
+
+});
+
+router.get('/insert', (req,res) => {
+
+});
+
+router.get('/update', async (req,res) => {
+
+});
+
+router.get('/delete', async (req,res) => {
+
+});
+
+router.get('/details', async (req,res) => {
+
+});
+
+router.post('/login', async (req,res) => {
+
+});
+
+router.post('/logout', (req,res) => {
+
+});
+
+router.post('/insert', async (req,res) => {
+
+});
+
+router.post('/update', async (req,res) => {
+
+});
+
+router.post('/delete', async (req,res) => {
+
+});
