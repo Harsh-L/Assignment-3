12b0911c14d75932f3194244dd60c6d5812275ec
Harsh Lad <68841816+Harsh-L@users.noreply.github.com>
2022-10-29 17:02:28 +0530

    update functionality added

--- Assignment 3.2/Frontend/src/app/update/update.component.ts
+++ Assignment 3.2/Frontend/src/app/update/update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-update',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update.component.css']
 })
 export class UpdateComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  product: any = {};
+  angForm: FormGroup;
+  constructor(private route: ActivatedRoute, private router: Router, private fb:FormBuilder, private ps: ProductService) { 
+    this.angForm = this.fb.group({
+      name: ['', Validators.required],
+      description: ['', Validators.nullValidator],
+      price: ['', Validators.required],
+      details: ['', Validators.required],
+      seller: ['', Validators.required],
+      images: [[''], Validators.required],
+    })
   }
 
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.ps.editProduct(params['id']).subscribe(res => {
+        this.product = res;
+      })
+    })
+  }
+
+  updateProduct(){
+    this.ps.updateProduct(this.product._id,
+      this.angForm.value.name, 
+      this.angForm.value.description, 
+      this.angForm.value.price, 
+      this.angForm.value.details,
+      this.angForm.value.seller,
+      this.angForm.value.images);
+
+      this.router.navigate(['/']);
+  }
 }
