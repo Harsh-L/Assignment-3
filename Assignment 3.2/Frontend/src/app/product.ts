74a2fba413d69ab260f549997305beb9ae782358
Harsh Lad <68841816+Harsh-L@users.noreply.github.com>
2022-10-29 10:33:16 +0530

    Product class created

---
+++ Assignment 3.2/Frontend/src/app/product.ts
@@ -0,0 +1,10 @@
+export default class Product{
+    name: String='';
+    description: String = '';
+    price: Number = 0;
+    details: String = '';
+    seller: String = '';
+    images: String = '';
+    rating: Number = 0;
+    ratingCount: Number = 0;
+}
